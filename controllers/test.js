b8626554bbaa3cd772e9cb63d410cd26f343eeac
Flavida <edgardgodoy5@gmail.com>
2021-10-06 20:04:19 -0300

    se agrego la carpeta controladores

---
+++ controllers/test.js
@@ -0,0 +1,25 @@
+const testGet=(req,res)=>{
+    res.json({
+        "mcg":"get api"
+    })
+}
+
+const testPost=(req,res)=>{
+    res.json({
+        "mcg":"get api"
+    })
+}
+
+const testPut=(req,res)=>{
+    res.json({
+        "mcg":"get api"
+    })
+}
+
+const testDelete=(req,res)=>{
+    res.json({
+        "mcg":"get api"
+    })
+}
+
+module.exports={testGet,testPost,testPut,testDelete}
